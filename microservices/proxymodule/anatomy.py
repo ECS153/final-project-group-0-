c8f9eebb451ced5f01b1f91816d89876ea505664
Raza Ahmed <rzahmed@ucdavis.edu>
2020-05-18 19:14:03 -0700

    proxy basic working

--- microservices/proxymodule/anatomy.py
+++ microservices/proxymodule/anatomy.py
@@ -1,23 +1,42 @@
 from mitmproxy import http, ctx
 import requests 
 
+import pyodbc
 
-
-def __init__(self):
+# database 
+#def __init__(self):
     # DATABASE VARIABLES
-    server = '192.168.1.4,1433' 
-    database = 'tempdb' 
-    username = 'sa' 
-    password = 'Anac0nda' 
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-    cursor = cnxn.cursor()
+server = '192.168.1.4' 
+database = 'secure' 
+username = 'sa' 
+password = 'Anac0nda' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
 
+#basic query
 
-# MITM REQUEST
+cursor.execute("SELECT * FROM ProxySwaps;")
+row = cursor.fetchone()
+
+# would be updated in the database per each request via extension
+generated_email = 'abcdefg'
+generated_pass = '123456789'
+actual_email = ""
+actual_pass = ""
+
+# get actual creds given generated
+while row: 
+    if (row[3] == generated_email):
+        actual_email = row[4]
+    if (row[3] == generated_pass):
+        actual_pass = row[4]
+    row = cursor.fetchone()
+
+#MITM REQUEST
 def request(flow: http.HTTPFlow) -> None:
     #ctx.log.info("url: %s\n" % flow.request.pretty_url)
     if flow.request.pretty_url == "https://www.ebay.com/signin/s" and flow.request.method == "POST":
     
-        flow.request.content = flow.request.content.replace(b'abcdefg',b'martin.petrov.1096@gmail.com')
-        flow.request.content = flow.request.content.replace(b'123456789', b'0nef0rallf0r0ne')
+        flow.request.content = flow.request.content.replace(bytes(generated_email, encoding='utf-8'),bytes(actual_email, encoding='utf-8'))
+        flow.request.content = flow.request.content.replace(bytes(generated_pass, encoding='utf-8'), bytes(actual_pass, encoding='utf-8'))
         ctx.log.info("GDFSFDSFDS url: %s\n" % flow.request.content)
--- microservices/proxymodule/python-mssql/database-example.py
+++ microservices/proxymodule/python-mssql/database-example.py
@@ -2,16 +2,17 @@ import pyodbc
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
-server = 'localhost' 
-database = 'tempdb' 
+server = '192.168.1.4' 
+database = 'secure' 
 username = 'sa' 
 password = 'Anac0nda' 
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
 #Sample select query
-cursor.execute("SELECT @@version;") 
-row = cursor.fetchone() 
-while row: 
-    print(row[0])
+cursor.execute("SELECT * FROM ProxySwaps;") 
+row = cursor.fetchone()  
+
+while row:
+    print(row[3])
     row = cursor.fetchone()
